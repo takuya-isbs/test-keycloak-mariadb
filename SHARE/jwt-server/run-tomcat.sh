892c9125d9aa9ccd47f447784032da071407492b
takuya-isbs <57710912+takuya-isbs@users.noreply.github.com>
2024-05-22 22:22:29 +0900

    jwt-server (tomcat): set org.apache.catalina.valves.RemoteIpValve

--- SHARE/jwt-server/run-tomcat.sh
+++ SHARE/jwt-server/run-tomcat.sh
@@ -6,6 +6,11 @@ set -x
 sh /jwt-server-sql.sh
 sh /update-ca.sh
 
+TOMCAT_SERVERXML=/etc/tomcat/server.xml
+
+# set "https" for redirect_uri from Keycloak
+sed -i '/<\/Host>/i \        <Valve className="org.apache.catalina.valves.RemoteIpValve" protocolHeader="x-forwarded-proto"/>' $TOMCAT_SERVERXML
+
 . /etc/tomcat/tomcat.conf
 . /etc/sysconfig/tomcat
 /usr/libexec/tomcat/server start
