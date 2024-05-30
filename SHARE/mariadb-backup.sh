e0fbd699f06152e55a8f171cdef5fc2f6440ada2
takuya-isbs <57710912+takuya-isbs@users.noreply.github.com>
2024-05-30 11:00:46 +0000

    mariadb-backup.sh: print filename

--- SHARE/mariadb-backup.sh
+++ SHARE/mariadb-backup.sh
@@ -5,7 +5,10 @@ set -x
 # REF.: https://mariadb.com/kb/en/container-backup-and-restoration/
 mkdir -p BACKUP
 DT=$(date +%Y%m%d-%H%M)
+FILE=./BACKUP/backup-mariadb-${DT}.gz
 
 docker compose exec mariadb \
        mariadb-dump --single-transaction --all-databases -uroot \
-    | gzip > ./BACKUP/backup-mariadb-${DT}.gz
+    | gzip > $FILE
+
+echo "DONE: $FILE"
