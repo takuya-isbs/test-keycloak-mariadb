c5adde2a601b12fae4f774742707069e7188e1e3
takuya-isbs <57710912+takuya-isbs@users.noreply.github.com>
2024-11-13 14:32:51 +0900

    update minica.sh

--- SHARE/minica.sh
+++ SHARE/minica.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+set -eu
+set -x
 
 UIDGID="$1"
+CERTS_DIR=/SHARE/certs
 
-MINICA="docker run --user $UIDGID -it --rm -v /SHARE/certs:/output ruanbekker/minica"
+#MINICA="docker run --user $UIDGID -it --rm -v ${CERTS_DIR}:/output ruanbekker/minica"
+MINICA="docker run --user $UIDGID -it --rm -v ${CERTS_DIR}:/output ryantk/minica"
+
+if [ -f ${CERTS_DIR}/minica-key.pem ]; then
+    exit 0
+fi
 
 $MINICA --domains keycloak.example.org,jwtserver.example.org
 $MINICA --domains dummy.example.org
